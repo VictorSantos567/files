5a4fe4a936621c44a4dced145cb0415a436db067
VictorSantos567 <victoraugustos49@gmail.com>
2021-01-25 20:08:53 -0300

    Implementação do Program e correção nas entidadades e metodos

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,80 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Items;
+
+//Fazer um programa para ler o caminho de um arquivo .csv
+//contendo os dados de itens vendidos. Cada item possui um
+//nome, preço unitário e quantidade, separados por vírgula. Você
+//deve gerar um novo arquivo chamado "summary.csv", localizado
+//em uma subpasta chamada "out" a partir da pasta original do
+//arquivo de origem, contendo apenas o nome e o valor total para
+//aquele item (preço unitário multiplicado pela quantidade),
+//conforme exemplo
+
 public class Program {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		List<Items> list = new ArrayList<>();
+
+		// D:\ws_arquivos\file.csv
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+
+		File path = new File(strPath);
+		String sourcePath = path.getParent();
+
+		boolean sucess = new File(sourcePath + "\\out").mkdir();
+
+		String targetFile = sourcePath + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(strPath))) {
+			
+			String itemCsv = br.readLine();
+			
+			while (itemCsv != null) {
+				String[] fields = itemCsv.split(";");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Items(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))) {
+
+				for (Items item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.amount()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFile + " CREATED!");
+
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+
+		sc.close();
 	}
 
 }
---
+++ src/entities/Items.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Items {
+	private String name;
+	private Double unitPrice;
+	private Integer quantity;
+
+	public Items() {
+	}
+
+	public Items(String name, Double unitPrice, Integer quantity) {
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double amount() {
+		return unitPrice * quantity;
+	}
+
+
+}
